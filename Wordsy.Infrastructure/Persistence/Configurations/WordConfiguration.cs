5e6728b3b18b37ffdcda6e94dd31026ec8df1711
vitaliinnest <vitaliinnest@gmail.com>
2025-04-04 17:41:29 +0300

    Add WordConfiguration for Entity Framework setup
    Introduces the `WordConfiguration` class implementing
    `IEntityTypeConfiguration<Word>`. Configures `Text` and
    `Translation` properties to be required with a maximum
    length of 100 characters.

---
+++ Wordsy.Infrastructure/Persistence/Configurations/WordConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Wordsy.Domain.Entities;
+
+namespace Wordsy.Infrastructure.Persistence.Configurations;
+
+public class WordConfiguration : IEntityTypeConfiguration<Word>
+{
+	public void Configure(EntityTypeBuilder<Word> builder)
+	{
+		builder
+			.Property(w => w.Text)
+			.IsRequired()
+			.HasMaxLength(100);
+
+		builder
+			.Property(w => w.Translation)
+			.IsRequired()
+			.HasMaxLength(100);
+	}
+}
