83fb5732a49c0f86dfacd6f740f6aed53c8690ad
vitaliinnest <vitaliinnest@gmail.com>
2025-04-06 13:08:34 +0300

    Add Azure AD B2C authentication support
    Updated `Program.cs` to configure Azure AD B2C authentication, including necessary middleware for authentication and authorization. Added package references in `Wordsy.WebApi.csproj` for JWT and Microsoft Identity support. Updated `appsettings.json` to include Azure AD B2C configuration settings.

--- Wordsy.WebApi/Program.cs
+++ Wordsy.WebApi/Program.cs
@@ -1,16 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 using Wordsy.Application;
 using Wordsy.Infrastructure;
 using Wordsy.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddPresentation();
+builder.Services
+	.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+
+builder.Services
+	.AddApplication()
+	.AddInfrastructure(builder.Configuration)
+	.AddPresentation();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
